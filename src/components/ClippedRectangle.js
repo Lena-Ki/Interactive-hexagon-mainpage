52796cab53c5b72dbd32410216ef63c7d175abd8
Helen Kim <lenahomerus@gmail.com>
2020-08-31 02:54:08 +0300

    team info rectangle added

---
+++ src/components/ClippedRectangle.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './clipped.css'
+
+export const ClippedRectangle = () => {
+  return (
+    <div className="cut-corner"></div>
+  )
+}
--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,14 +1,15 @@
 import React, { useState } from 'react'
 import { AppStore } from '../stores/AppStore'
+import { ClippedRectangle } from './ClippedRectangle'
 import { ReusableHex } from './HexItems/ReusableHex'
 import './maincontent.css'
+import './hexcontent.css';
 
 export const MainContent = () => {
 
   const [active, setActive] = useState(3)
 
   const renderHex = (active, device) => {
-    console.log(active, device)
     let Hexs = []
     for (let i = 0; i < 5; i++) {
       let classId = AppStore.states.classid[active - 1][i]
@@ -24,10 +25,20 @@ export const MainContent = () => {
     return Hexs
   }
 
-  return (
-    <div className="hex-container">
-      {renderHex(active, 'desktop')}
-    </div>
+  const renderTeams = (active) => {
+    let team1 = AppStore.items[active - 1].team1
+    console.log(team1)
+    return (<span className="mainhex__date team1">{team1}</span>)
+  }
 
+  return (
+    <>
+      <div className="cut-corner">
+        {renderTeams(active)}
+      </div>
+      <div className="hex-container">
+        {renderHex(active, 'desktop')}
+      </div>
+    </>
   )
 }
