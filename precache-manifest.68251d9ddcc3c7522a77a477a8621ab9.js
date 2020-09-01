aa9708393fd77a5866c0ee9f08d97627e89c0734
Helen Kim <lenahomerus@gmail.com>
2020-09-01 12:11:47 +0300

    Updates

---
+++ precache-manifest.68251d9ddcc3c7522a77a477a8621ab9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38f64fae480571487353f8fd450ee254",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3920aa344c0249a4ccca",
+    "url": "/static/css/main.e27982b1.chunk.css"
+  },
+  {
+    "revision": "3e49b6886b3747162577",
+    "url": "/static/js/2.d9d5d184.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d9d5d184.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3920aa344c0249a4ccca",
+    "url": "/static/js/main.3592eca3.chunk.js"
+  },
+  {
+    "revision": "5d46deb968c00a794048",
+    "url": "/static/js/runtime-main.6472e494.js"
+  },
+  {
+    "revision": "55e18b2a88c7f38e5643c96fa383ebb6",
+    "url": "/static/media/bg-circle.55e18b2a.svg"
+  },
+  {
+    "revision": "1d4af823b6c98c9d62da17b008d6d439",
+    "url": "/static/media/clipped-rect.1d4af823.svg"
+  },
+  {
+    "revision": "538ce11023e2754fe0ea7950e3c7990f",
+    "url": "/static/media/hex.538ce110.svg"
+  }
+]);
